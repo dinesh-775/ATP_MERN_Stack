0487b76852f1f7c0074b437bddab4ae478bc9630
dinesh-775 <nenavathdinesh.08@gmail.com>
2026-01-26 15:58:03 +0530

    commit

---
+++ Day4_JS/notes.js
@@ -0,0 +1,21 @@
+// Javascript is a single threaded  program
+// A thread can do one task at a time
+/*
+
+Most frequent Async operations in node.js
+*javascript can run on node.js/Browser
+*JavaScript Engin(Single thread):
+1. Call back
+2.Heap Memory
+*Web API
+browsers javascript run time = JavaScript Engin + Event loops
+node js run time = Javascript API + Event loops
+
+1 sync blocking means the code which stops the execution of further code until the current code is executed
+
+2 sync non blocking means the code which does not stops the execution of further code until the current code is executed
+
+3 Async blocking means the code which stops the execution of further code until the current code is executed but it will be executed after some time
+
+4 Async Non blocking means the code which does not stops the execution of further code until the current code is executed and it will be executed after some time
+*/
